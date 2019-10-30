9c77545a389899dfe19931a62eef59c874b1c3a1
José Pedro Oliveira <jpedrooliveira.w@gmail.com>
2019-10-30 11:53:00 +0000

    update post models

--- wanna-backend/src/api/validations/post.validation.js
+++ wanna-backend/src/api/validations/post.validation.js
@@ -4,8 +4,11 @@ module.exports = {
 	// POST /v1/post/createPost
 	createPost: [
 		body('description', 'Description is invalid').exists(),
-		body('price'),
+		body('price', 'Price is required').exists(),
 		body('isAvailable', 'Status is required').exists(),
+		body('category', 'Category is required').exists(),
+		body('color', 'Color is required').exists(),
+		body('size', 'Size is required').exists(),
 	],
 
 	//POST /v1/post/createUserPost
