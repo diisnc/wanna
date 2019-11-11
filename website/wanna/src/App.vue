da11dab8ac8eeee6beab434d92cff58b0f6dcede
Vitor Peixoto <vitor-peixoto@outlook.pt>
2019-11-11 00:21:53 +0000

    modal for report posts and other options website

--- website/wanna/src/App.vue
+++ website/wanna/src/App.vue
@@ -40,11 +40,11 @@ export default {
   }
 
   .popover {
-    margin-top: 17px !important;
+    margin-top: 18px !important;
     border: 0px !important;
     border-radius: 20px !important;
     opacity: inherit !important;
-    -webkit-box-shadow: 0px 15px 20px 2px rgba(0,0,0,0.15) !important;
+    -webkit-box-shadow: 0px 11px 32px -9px rgba(28,64,222,0.25) !important;
     display: block !important;
     visibility: visible !important;
   }
@@ -60,10 +60,6 @@ export default {
     cursor: pointer !important;
     border: 0px !important;
     display: block !important;
-  }
-
-  .arrow{
-    border: 0px !important;
   }
 
   .page{
