d3ecf4b39d8cce3ba31f458338fb2f50d0e98af1
Sérgio Jorge <stoj97@gmail.com>
2019-12-05 20:10:23 +0000

    adding animations

--- WannaApp/screens/Main.js
+++ WannaApp/screens/Main.js
@@ -16,119 +16,183 @@ import { createMaterialTopTabNavigator, BottomTabBar } from 'react-navigation-ta
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Animated, Easing } from 'react-native';
 
-const FilterStack = createStackNavigator({
-	Filters: {
-		screen: Filters,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	Wanted: {
-		screen: Wanted,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	NewFilter: {
-		screen: NewFilter,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	}
-});
+const transitionConfig = () => {
+	return {
+		transitionSpec: {
+			duration: 800,
+			easing: Easing.out(Easing.poly(4)),
+			timing: Animated.timing,
+			useNativeDriver: true
+		},
+		screenInterpolator: sceneProps => {
+			const { position, layout, scene, index, scenes } = sceneProps;
 
-const UserStack = createStackNavigator({
-	MyProfile: {
-		screen: Profile,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	EditProfile: {
-		screen: EditProfile,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	UserProfile: {
-		screen: Profile,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	UserPostProfile: {
-		screen: UserPostProfile,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	FollowList: {
-		screen: FollowList,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	Comments: {
-		screen: Comments,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	ConversationsList: {
-		screen: ConversationsList,
-		navigationOptions: {
-			header: null
-		}
-	},
-	Chat: {
-		screen: Chat,
-		navigationOptions: {
-			header: null
-		}
-	}
-});
+			const thisSceneIndex = scene.index;
+			const height = layout.initHeight;
+			const width = layout.initWidth;
 
-const FeedStack = createStackNavigator({
-	MyFeed: {
-		screen: Inspire,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
+			// We can access our navigation params on the scene's 'route' property
+			var thisSceneParams = scene.route.params || {};
+
+			const translateX = position.interpolate({
+				inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+				outputRange: [width, 0, 0]
+			});
+
+			const translateY = position.interpolate({
+				inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+				outputRange: [height, 0, 0]
+			});
+
+			const opacity = position.interpolate({
+				inputRange: [thisSceneIndex - 1, thisSceneIndex - 0.5, thisSceneIndex],
+				outputRange: [0, 1, 1]
+			});
+
+			const scale = position.interpolate({
+				inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
+				outputRange: [4, 1, 1]
+			});
+
+			const slideFromRight = { transform: [{ translateX }] };
+			const scaleWithOpacity = { opacity, transform: [{ scaleX: scale }, { scaleY: scale }] };
+			const slideInFromBottom = { transform: [{ translateY }] };
+
+			if (thisSceneParams.plain) return slideFromRight;
+			else if (index < 5) return slideInFromBottom;
+			else return scaleWithOpacity;
+		}
+	};
+};
+
+const FilterStack = createStackNavigator(
+	{
+		Filters: {
+			screen: Filters,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		Wanted: {
+			screen: Wanted,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		NewFilter: {
+			screen: NewFilter,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
 		}
 	},
-	UserProfile: {
-		screen: Profile,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	UserPostProfile: {
-		screen: UserPostProfile,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
-	},
-	Comments: {
-		screen: Comments,
-		headerMode: 'none',
-		navigationOptions: {
-			header: null
-		}
+	{
+		transitionConfig
 	}
-});
+);
+
+const UserStack = createStackNavigator(
+	{
+		MyProfile: {
+			screen: Profile,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		EditProfile: {
+			screen: EditProfile,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		UserProfile: {
+			screen: Profile,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		UserPostProfile: {
+			screen: UserPostProfile,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		FollowList: {
+			screen: FollowList,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		Comments: {
+			screen: Comments,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		ConversationsList: {
+			screen: ConversationsList,
+			navigationOptions: {
+				header: null
+			}
+		},
+		Chat: {
+			screen: Chat,
+			navigationOptions: {
+				header: null
+			}
+		}
+	},
+	{
+		transitionConfig
+	}
+);
+
+const FeedStack = createStackNavigator(
+	{
+		MyFeed: {
+			screen: Inspire,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		UserProfile: {
+			screen: Profile,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		UserPostProfile: {
+			screen: UserPostProfile,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		},
+		Comments: {
+			screen: Comments,
+			headerMode: 'none',
+			navigationOptions: {
+				header: null
+			}
+		}
+	},
+	{
+		transitionConfig
+	}
+);
 
 const MainTab = createMaterialTopTabNavigator(
 	{
